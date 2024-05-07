ffc8e14b1563ddebf00e8a9e1ba60eb9bce06215
Monika <lumimonika1@gmail.com>
2024-05-07 01:36:33 -1100

    style the pdf generate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill.
@@ -30,15 +33,47 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def generate(self, flatmates1, flatmates2, bill):
-        pass
+    def generate(self, flatmate1, flatmate2, bill):
 
-the_bill = Bill(amount=120,period="March 2021")
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        #Add icon
+        pdf.image("house.png", w=40, h=40)
+
+        #Insert title
+        pdf.set_font(family='Times', size=20, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        #Insert Period label and value
+        pdf.set_font(family="Times", size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        #Insert name and due amount of the first flatname
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        #Insert name and due amount of the first flatname
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
+
+
+the_bill = Bill(amount=120, period="April 2021")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
 print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
 print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
 
 
 
