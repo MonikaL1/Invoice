cf5e30f39c8afea249bda850a09a322e05dfac30
Monika <lumimonika1@gmail.com>
2024-05-09 02:15:54 -1100

    Building a CLI

--- main.py
+++ main.py
@@ -69,19 +69,25 @@ class PdfReport:
         #Automatically View a PDF file
         webbrowser.open(self.filename)
 
-a = float(input("Hey user, enter the bill amount: "))
-print("This is a ", a)
+amount = float(input("Hey user, enter the bill amount: "))
+period = input("What is the bill period? E.g. : December 2020: ")
+
+name1 = input("What is your name? ")
+days_in_house1 = int(input(f"How many days did {name1} stay in the haouse during the bill period? "))
+
+name2 = input("What is the name of the other flatmate? ")
+days_in_house2 = int(input(f"How many days did {name2} stay in the haouse during the bill period? "))
 
 
-the_bill = Bill(amount=a, period="April 2021")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
+the_bill = Bill(amount, period)
+flatmate1 = Flatmate(name1, days_in_house1)
+flatmate2 = Flatmate(name2, days_in_house2)
 
-print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
-print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
+print(f"{flatmate1.name} pays: ", flatmate1.pays(the_bill, flatmate2))
+print(f"{flatmate2.name} pays: ", flatmate2.pays(the_bill, flatmate1))
 
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1, flatmate2, bill=the_bill)
 
 
 
